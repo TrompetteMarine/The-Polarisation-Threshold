f2398e635ae3fd467eb1d0fc63250010c0ab67b3
Gabriel LS Bontemps <102915139+TrompetteMarine@users.noreply.github.com>
2026-07-18 10:25:11 +0100

    Keep Julia test target focused on the dependency-free numerical core

--- CIT/julia/src/CITReplication.jl
+++ CIT/julia/src/CITReplication.jl
@@ -5,18 +5,14 @@ include("Model.jl")
 include("MonteCarlo.jl")
 include("Statistics.jl")
 include("Generator.jl")
-include("Plotting.jl")
-include("Pipeline.jl")
 
 using .Types
 using .Model
 using .MonteCarlo
 using .KernelStatistics
 using .GeneratorCheck
-using .CITPlotting
-using .Pipeline
 
 export ModelParameters, SimulationParameters, ResponseResult, KernelSummary, GeneratorResult
-export paired_response, summarize_kernel, laplace_transform, generator_cross_check, run_pipeline
+export paired_response, summarize_kernel, laplace_transform, generator_cross_check
 
 end
