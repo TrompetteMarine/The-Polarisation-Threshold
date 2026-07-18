88826934ca4d63b5b9fc417aa653394fa010687a
Gabriel LS Bontemps <102915139+TrompetteMarine@users.noreply.github.com>
2026-07-18 10:29:14 +0100

    Expose Julia core API from the package root

--- CIT/julia/src/CITReplication.jl
+++ CIT/julia/src/CITReplication.jl
@@ -6,11 +6,10 @@ include("MonteCarlo.jl")
 include("Statistics.jl")
 include("Generator.jl")
 
-using .Types
-using .Model
-using .MonteCarlo
-using .KernelStatistics
-using .GeneratorCheck
+using .Types: ModelParameters, SimulationParameters
+using .MonteCarlo: ResponseResult, paired_response
+using .KernelStatistics: KernelSummary, summarize_kernel, laplace_transform
+using .GeneratorCheck: GeneratorResult, generator_cross_check
 
 export ModelParameters, SimulationParameters, ResponseResult, KernelSummary, GeneratorResult
 export paired_response, summarize_kernel, laplace_transform, generator_cross_check
